acac841e18336ff7ee6b4d5e9537a1d3f51e5346
chysis <chysiss@naver.com>
2025-08-29 21:58:03 +0900

    refactor: index에서 바로 router 렌더링하도록 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { ConfigProvider } from 'antd';
+import koKR from 'antd/locale/ko_KR';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ConfigProvider
+      locale={koKR}
+      theme={{
+        token: {
+          colorPrimary: '#0064FF',
+        },
+      }}
+    >
+      <RouterProvider router={router} />
+    </ConfigProvider>
+  </StrictMode>
+);
