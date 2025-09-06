bdebe0264ffcf0720f20d66ee4e1494c484a68b0
chysis <chysiss@naver.com>
2025-09-07 01:12:51 +0900

    feat: 프로필 심사 상세 페이지 구현

--- src/router.tsx
+++ src/router.tsx
@@ -2,29 +2,33 @@ import DefaultLayout from '@/components/layouts/DefaultLayout';
 import LoginPage from '@/pages/LoginPage';
 import { createBrowserRouter } from 'react-router-dom';
 import ProfileReviewPage from './pages/ProfileReview';
+import ProfileReviewDetailPage from './pages/ProfileReviewDetail';
 
 export const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <LoginPage />,
-  },
-  {
-    path: '/admin',
-    element: <DefaultLayout />,
-    children: [
-      {
-        index: true,
-        element: <div>Home</div>,
-      },
-      {
-        path: 'users',
-        element: <div>Users</div>,
-      },
-      {
-        path: 'profile-review',
-        element: <ProfileReviewPage />,
-        children: [],
-      },
-    ],
-  },
+	{
+		path: '/',
+		element: <LoginPage />,
+	},
+	{
+		path: '/admin',
+		element: <DefaultLayout />,
+		children: [
+			{
+				index: true,
+				element: <div>Home</div>,
+			},
+			{
+				path: 'users',
+				element: <div>Users</div>,
+			},
+			{
+				path: 'profile-review',
+				element: <ProfileReviewPage />,
+			},
+			{
+				path: 'profile-review/:tempInstructorId',
+				element: <ProfileReviewDetailPage />,
+			},
+		],
+	},
 ]);
