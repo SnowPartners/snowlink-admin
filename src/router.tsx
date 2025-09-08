761bc0f46336bddfc9e84d1c3d07743916c126c7
chysis <chysiss@naver.com>
2025-09-08 23:16:28 +0900

    feat: 대시보드 화면 수정

--- src/router.tsx
+++ src/router.tsx
@@ -3,32 +3,33 @@ import LoginPage from '@/pages/LoginPage';
 import { createBrowserRouter } from 'react-router-dom';
 import ProfileReviewPage from './pages/ProfileReview';
 import ProfileReviewDetailPage from './pages/ProfileReviewDetail';
+import Title from 'antd/es/typography/Title';
 
 export const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <LoginPage />,
-	},
-	{
-		path: '/admin',
-		element: <DefaultLayout />,
-		children: [
-			{
-				index: true,
-				element: <div>Home</div>,
-			},
-			{
-				path: 'users',
-				element: <div>Users</div>,
-			},
-			{
-				path: 'profile-review',
-				element: <ProfileReviewPage />,
-			},
-			{
-				path: 'profile-review/:tempInstructorId',
-				element: <ProfileReviewDetailPage />,
-			},
-		],
-	},
+  {
+    path: '/',
+    element: <LoginPage />,
+  },
+  {
+    path: '/admin',
+    element: <DefaultLayout />,
+    children: [
+      {
+        index: true,
+        element: <Title level={3}>스노우링크 관리자 페이지입니다.</Title>,
+      },
+      {
+        path: 'users',
+        element: <div>Users</div>,
+      },
+      {
+        path: 'profile-review',
+        element: <ProfileReviewPage />,
+      },
+      {
+        path: 'profile-review/:tempInstructorId',
+        element: <ProfileReviewDetailPage />,
+      },
+    ],
+  },
 ]);
